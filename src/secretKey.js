fe579d813527f5e5ab9592fd538666cc7add8073
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-23 08:41:30 -0600

    done

---
+++ src/secretKey.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  return {
+    darksky: "ebb863b9dcae16adee0ccebbc207b50a",
+    mapbox:
+      "pk.eyJ1IjoicmVuYW56IiwiYSI6ImNqdXNqbDh1djFqdzE0ZHJ2dXByeWNxdTMifQ.VIHFWg4Xc8aXFDSa935JsA"
+  };
+};
