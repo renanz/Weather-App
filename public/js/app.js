3f2dd2ed8dde75aa1942ae69c5807d5cf7f827ca
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-23 08:37:36 -0600

    ready for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(res => {
+  fetch(`/weather?address=${search.value}`).then(res => {
     res.json().then(data => {
       if (data.error) messageOne.textContent = data.error;
       else {
